b97f0de6261fde0f8a4b32177243ab7cd3fcd14d
Tom Smith <me@tomm.us>
2019-05-22 20:49:07 +0100

    Fix import in Schema

--- Schema.go
+++ Schema.go
@@ -1,6 +1,6 @@
 package exodus
 
-import "github.com/gostalt/exodus/column"
+import "github.com/tmus/exodus/column"
 
 // Schema is a slice of items that satisfy the Columnable interface.
 type Schema []column.Columnable
