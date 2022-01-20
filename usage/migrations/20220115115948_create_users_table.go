f17f3c5dc4acfbc4c03e7dde14b6d8f60794c0f2
Thomas Smith <me@tomm.us>
2022-01-20 08:43:19 +0000

    wip

--- usage/migrations/20220115115948_create_users_table.go
+++ usage/migrations/20220115115948_create_users_table.go
@@ -10,7 +10,7 @@ type migration20220115115948create_users_table struct{}
 func (migration20220115115948create_users_table) Up(schema *exodus.MigrationPayload) {
 	schema.Create("example", []column.Definition{
 		column.Increments("id"),
-		column.String("username", 255).Nullable().Default("greenhorn"),
+		column.String("display_name", 255).Nullable().Default("greenhorn"),
 		column.String("email", 100).Unique(),
 		column.Boolean("is_verified"),
 		column.Date("created_at"),
